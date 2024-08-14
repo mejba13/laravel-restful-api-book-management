b3d428f522a322f8d6ac5c5c1ddb111c096609d3
mejba13 <mejba.13@gmail.com>
2024-08-14 12:53:35 +0600

    Initialize Laravel project with basic Book API setup

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'author' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+        ];
+    }
+}
